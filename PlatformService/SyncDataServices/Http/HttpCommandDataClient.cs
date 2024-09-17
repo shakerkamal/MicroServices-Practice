c95ccbe406f57fac2731535641790030bcad0337
shakerkamal <shakerkamal18@gmail.com>
2024-09-17 08:43:37 +0200

    Creates production environment support and introduces synchoronous commnunication with another service

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,35 @@
+using System.Text;
+using System.Text.Json;
+using PlatformService.Dtos;
+
+namespace PlatformService.SyncDataServices.Http;
+
+public class HttpCommandDataClient : ICommandDataClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+
+    public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _configuration = configuration;
+    }
+
+    public async Task SendPlatformCommand(PlatformReadDto platformReadDto)
+    {
+        var httpContent = new StringContent(
+            JsonSerializer.Serialize(platformReadDto),
+            Encoding.UTF8,
+            "application/json");
+
+        var response  = await _httpClient.PostAsync($"{_configuration["CommandService"]}", httpContent);
+        if(response.IsSuccessStatusCode)
+        {   
+            Console.WriteLine("---> Sync Post to Command Service");
+        }
+        else
+        {
+            Console.WriteLine("---> failed!!");
+        }
+    }
+}
